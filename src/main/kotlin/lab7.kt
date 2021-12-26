2e65040124bf48864508d6aa8797af92d5c8ded6
JaneWaker <58956682+JaneWaker@users.noreply.github.com>
2021-12-27 02:43:43 +0300

    l7 update

--- src/main/kotlin/lab7.kt
+++ src/main/kotlin/lab7.kt
@@ -24,7 +24,7 @@ object SerializeShapes {
         return json.encodeToString(inList)
     }
 
-    fun decode(inString: String): List<Shape> {
+    fun decode(inString: String): MutableList<Shape> {
         return json.decodeFromString(inString)
     }
 }
